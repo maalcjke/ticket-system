38f78fa95c3e4d6e8b813b6b24a67fc39d93c7b7
maalcjke <malc.jke@yandex.ru>
2025-05-14 16:26:20 +0300

    fix: create

--- src/tickets/create/create.ts
+++ src/tickets/create/create.ts
@@ -2,15 +2,17 @@ import * as tickets from './queries/create.queries.js'
 import { db } from '@core/storage/index.js'
 import { TicketStatus } from '@core/enums.js'
 
-export const create = async (param: Omit<tickets.ICreateParams, 'status'>) => {
-  const { title, body, reason = '' } = param
+export const create = async (
+  param: Omit<tickets.ICreateParams, 'status' | 'reason'>,
+) => {
+  const { title, body } = param
 
   return await tickets.create
     .run(
       {
         title,
         body,
-        reason,
+        reason: '',
         status: TicketStatus.NEW,
       },
       db,
